5f0ff496d8e8b022807de21651dfd886b1e2f4a6
unknown <hungle.dut@gmail.com>
2023-08-10 11:29:39 +0700

    add logger

---
+++ logger/logger.py
@@ -0,0 +1,36 @@
+"Custom Logger"
+import logging
+
+def configure_logging():
+    """Initialize logging defaults for Project.
+
+    This function does:
+    - Assign INFO and DEBUG level to logger file handler and console handler.
+
+    Returns:
+        Logger.
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler with a lower log level
+    file_handler = logging.FileHandler('logger/logger.log')
+    file_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and add it to the handlers
+    default_formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(name)s] "
+        "[%(funcName)s():%(lineno)s] [PID:%(process)d TID:%(thread)d] %(message)s",
+        "%d/%m/%Y %H:%M:%S")
+
+    file_handler.setFormatter(default_formatter)
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+custom_logger = configure_logging()
--- main.py
+++ main.py
@@ -3,7 +3,37 @@ from fastapi import FastAPI
 from routes.users_routes import user_api_router
 from routes.login_routes import login_api_router
 
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from logger.logger import custom_logger
+import time
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    """Logging All API request"""
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        # start_time = time.time()
+        # Log the request
+        custom_logger.info("Received request: %s %s", request.method, request.url)
+        # custom_logger.debug("Request headers: %s", request.headers)
+        # custom_logger.debug("Request body: %s", await request.body())
+
+        # Call the next middleware or route handler
+        response = await call_next(request)
+        # process_time = time.time() - start_time
+        # response.headers['Process-Time'] = str(process_time)
+
+        # Log the response
+        custom_logger.info("Response status code: %s", response.status_code)
+        # custom_logger.debug("Response headers: %s", response.headers)
+
+        return response
+
+
 app = FastAPI()
 
 app.include_router(user_api_router)
 app.include_router(login_api_router)
+app.add_middleware(LoggingMiddleware)
